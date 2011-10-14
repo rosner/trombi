cd891742c941c9b2807b53605e0247bf1983239b
Daniel Truemper <truemped@googlemail.com>
2011-10-14 07:42:54 +0200

    The Session API only works when not in Admin Party
    So I added another setup() method that configures the couchdb with an
    admin user.

--- test/couch_util.py
+++ test/couch_util.py
@@ -43,6 +43,63 @@ from tornado.httpclient import HTTPClient
 
 baseurl = ''
 
+def setup_with_admin():
+    global _proc, baseurl
+    try:
+        shutil.rmtree('tmp')
+    except OSError:
+        # Python 3
+        err = sys.exc_info()[1]
+        if err.errno != errno.ENOENT:
+            raise
+
+    os.mkdir('tmp')
+    os.mkdir('tmp/couch')
+
+    dbdir = 'tmp/couch'
+    ini = 'tmp/local.ini'
+    log = 'tmp/couch.log'
+
+    port = 8921
+    baseurl = 'http://localhost:%d/' % port
+
+    with open(ini, 'w') as fobj:
+        fobj.write('''\
+[couchdb]
+database_dir = %(dbdir)s
+view_index_dir = %(dbdir)s
+
+[httpd]
+port = %(port)d
+bind_address = 127.0.0.1
+
+[log]
+file = %(log)s
+
+[admins]
+admin=admin
+''' % dict(dbdir=dbdir, log=log, port=port))
+
+    cmdline = 'couchdb -a %s' % ini
+    null = open('/dev/null', 'w')
+    _proc = subprocess.Popen(cmdline, shell=True, stdout=null, stderr=null)
+
+    # Wait for couchdb to start
+    time.sleep(1)
+    # Wait for couchdb to start
+
+    while True:
+        try:
+            f = request.urlopen(baseurl)
+        except URLError:
+            continue
+        try:
+            json.loads(f.read().decode('utf-8'))
+        except ValueError:
+            continue
+        # Got a sensible response
+        break
+
 def setup():
     global _proc, baseurl
     try:
